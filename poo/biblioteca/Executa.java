2475d702b2e082a4c751dd0ac6b54e0ef165d496
MarquinhoAmorim <marcoamorim877@gmail.com>
2024-05-10 19:44:01 -0300

    Tarefa Avaliativa

---
+++ poo/biblioteca/Executa.java
@@ -0,0 +1,53 @@
+package biblioteca;
+
+
+class Executa // classe de testes
+{
+    // método que executa programa
+    public static void main(String args[])
+    { 
+        /*
+            // criação de um objeto do tipo "Pessoa"
+            Pessoa jose = new Pessoa("Jose", "01/06/1975");
+        
+            // exibição dos atributos do objeto jose
+            System.out.println("Nome = " + jose.getNome() + " - Data de Nascimento = " + jose.getData());
+        */
+       
+        // criação de um objeto do tipo "Aluno"
+        Aluno maria = new Aluno("Maria","10/11/1976","111111","Computacao");
+        // atribui valores ao telefone de maria
+        maria.setFone("019","98765432");
+        // exibição dos atributos do objeto maria
+        System.out.println("Nome = " + maria.getNome() +
+        " - Data de Nascimento = " + maria.getData() +
+        " - RA = " + maria.getRa() +
+        " - Curso = " + maria.getCurso() );
+        // exibe o telefone de maria
+        System.out.println("Telefone de " + maria.getNome() + " = " + maria.getFone());
+        
+        
+        // criação de um objeto do tipo "Professor"
+        Professor pedro = new Professor("Pedro","05/05/1970","Mestre","02/02/2007");
+        // atribui valores ao telefone de pedro
+        pedro.setFone("019","12345678");
+        // exibição dos atributos do objeto pedro
+        System.out.println("Nome = " + pedro.getNome() +
+        " - Data de Nascimento = " + pedro.getData() +
+        " - Titulacao = " + pedro.getTitulacao() +
+        " - Contratado em = " + pedro.getDataContratacao() );
+        // exibe o telefone de pedro
+        System.out.println("Telefone de " + pedro.getNome() + " = " + pedro.getFone());
+        
+        Livro java = new Livro("Java basico","Livros e Cia","1111");
+        Livro oo = new Livro("Orientacao a Objetos","Livros e Cia","2222");
+        maria.retiraLivro( java );
+        maria.retiraLivro( oo );
+        maria.listaLivrosRetirados();
+        java.comQuemEsta();
+        oo.comQuemEsta();
+        maria.devolveLivro( java );
+        java.comQuemEsta();
+        pedro.listaLivrosRetirados();
+    }
+}
